57385d571dcfe736d999c571057a726af70cd147
Seth Fischer <seth@fischer.nz>
2020-02-10 22:14:48 +1300

    Consumption by month YYYY-mm

---
+++ reports/month.sql
@@ -0,0 +1,13 @@
+/**
+ * Usage by month.
+ */
+SELECT
+    TO_CHAR(started_at, 'YYYY-MM') AS year_month,
+    SUM(value) AS monthly_usage
+FROM
+    readings
+GROUP BY
+    year_month
+ORDER BY
+    year_month
+;
